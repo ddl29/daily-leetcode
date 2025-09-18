a01a82ab8536fe77d6b198dab779c8e1944b8694
Daniel Diaz <daniel.dd.diaz@oracle.com>
2025-09-17 23:50:31 -0600

    125. Valid Palindrome -- #two_pointers #string

---
+++ Easy/125_valid_palindrome.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    s = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+
+    let left = 0;
+    let right = s.length-1;
+    while (left < right) {
+        if (s.charAt(left) !== s.charAt(right))
+            return false;
+        left++;
+        right--;
+    }
+    return true;
+};
+
+console.log(isPalindrome("A man, a plan, a canal: Panama"));
